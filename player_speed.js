4401749a9eb7aafd243d81a1236c2a136c4f832c
Dying Ember <dyingember@proton.me>
2026-05-16 23:05:00 +0530

    Create player_speed.js

---
+++ player_speed.js
@@ -0,0 +1,11 @@
+/*:
+ * @plugindesc Increase default player move speed globally.
+ * @help
+ */
+(function(){
+  var _Game_Player_initMembers = Game_Player.prototype.initMembers;
+  Game_Player.prototype.initMembers = function(){
+    _Game_Player_initMembers.call(this);
+    this._moveSpeed = 5; // default is 4; set 5 or 6 for faster
+  };
+})();
