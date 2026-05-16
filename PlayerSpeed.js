60314316f02e84e30ae193a1d7b171969378ac4a
Dying Ember <dyingember@proton.me>
2026-05-16 23:58:28 +0530

    version 1

--- PlayerSpeed.js
+++ PlayerSpeed.js
@@ -7,10 +7,13 @@
     var _pluginDesc = "modify the player's movement speed";
     var parameters = PluginManger.parameters('SpeedBoost');
     var speedBonus = Number(parameters['SpeedBonus'] || 1.0);
+    var PlayerBaseSpeed = Game_CharacterBase.prototype.realMoveSpeed;
     
-}
-
-
-
-
-)
+    Game_CharacterBase.prototype.realMoveSpeed = function(){
+    var speed = PlayerBaseSpeed .call(this);
+    if (this === $gamePlayer){
+        return speed + speedBonus;
+    }
+    return speed;
+    };
+})();
