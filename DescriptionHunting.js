6beaccb76128e4043a61d2d6a22621f54b367f7f
Dying Ember <dyingember@proton.me>
2026-05-20 10:49:21 +0530

    descHunt update

--- DescriptionHunting.js
+++ DescriptionHunting.js
@@ -1,12 +1,13 @@
 //=============================================================================
 // Dynamic Parameter Extraction
 //=============================================================================
-// Purpose : RPG Maker Plugins look for their settings using the exact file name
-// like - "MyPlugin". Use it too look for them using description instead.
+// Purpose : Normally, RPG Maker Plugins look for their settings using the exact
+// file name like - "MyPlugin". Use it to look for them using their description
+// instead.
 //=============================================================================
 // Use : put it inside your plugin for which you want to use it. And copy paste
 // your plugin's desc in place of "Your plugin's description here". 
-// I should work now. If it doesn't. You may need to change it a bit.
+// I should work now.
 //=============================================================================
 
 // look for the plugin using '@desc' text
