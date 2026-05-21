af0202bcbc8bcfd25a19c48f16ccd2512e68d8e5
Dying Ember <dyingember@proton.me>
2026-05-21 16:49:15 +0530

    asdfqw

--- plugins/CustomTitleScreenMenu.js
+++ plugins/CustomTitleScreenMenu.js
@@ -1,34 +1,34 @@
 // ver - 1.0 final
 /*:
  * @plugindesc Customise title screen menu
- * 
+ *
  * @param NewGameText
  * @text New Game Text
  * @desc The text displayed for starting a new game
  * @default New Game
- * 
+ *
  * @param ContinueText
  * @text Continue Text
  * @desc The text displayed to load and continue the game
  * @default Continue
- * 
+ *
  * @param OptionsText
  * @text Options Text
- * @desc The text displayed for the options menu 
+ * @desc The text displayed for the options menu
  * @default Options
- * 
+ *
  * @param QuitText
  * @text Quit Text
  * @desc The text displayed for exiting the game
  * @default Quit
- * 
+ *
  * @help
  * ----------------------------------------------------
  * Overwrite the default text in the title screen menu
  * to customise the title creen menu
  * No plugin commands
  * ----------------------------------------------------
-*/ 
+ */
 
 (function(){
     'use strict';
@@ -37,8 +37,8 @@
     var txtContinue = String(parameters['ContinueText'] || 'Continue');
     var txtOptions = String(parameters['OptionsText'] || 'Options');
     var txtQuit = String(parameters['QuitText'] || 'Quit');
-    
-    // Overwriting the command list 
+
+    // Overwriting the command list
     Window_TitleCommand.prototype.makeCommandList = function(){
         this.addCommand(txtNewGame, 'newGame');
         this.addCommand(txtContinue, 'continue', this.isContinueEnabled());
